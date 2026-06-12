c0d54fd4d69dc3cb97828821f31849002da41f9a
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:09:57 +0530

    feat: add ApiError class for HTTP errors

---
+++ src/utils/ApiError.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Error type carrying an HTTP status code.
+ * Thrown by controllers/services and rendered by the error handler.
+ */
+class ApiError extends Error {
+  /**
+   * @param {number} statusCode - HTTP status code to send.
+   * @param {string} message - human readable message.
+   * @param {object} [details] - optional extra context.
+   */
+  constructor(statusCode, message, details) {
+    super(message);
+    this.name = 'ApiError';
+    this.statusCode = statusCode;
+    this.details = details;
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  /** Convenience factory for 400 Bad Request. */
+  static badRequest(message, details) {
+    return new ApiError(400, message, details);
+  }
+
+  /** Convenience factory for 404 Not Found. */
+  static notFound(message, details) {
+    return new ApiError(404, message, details);
+  }
+
+  /** Convenience factory for 409 Conflict. */
+  static conflict(message, details) {
+    return new ApiError(409, message, details);
+  }
+}
+
+module.exports = ApiError;
